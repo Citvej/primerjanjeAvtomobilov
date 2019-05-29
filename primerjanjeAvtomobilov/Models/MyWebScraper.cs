57a754c387d78ed1e5e9c4d88bf84963fee682fc
Neven Jevtic <neven.jevtic123@gmail.com>
2019-05-29 15:51:08 +0200

    Updated Web scraper, ki dobi podatke o enem avtu & prenese slike

--- primerjanjeAvtomobilov/Models/MyWebScraper.cs
+++ primerjanjeAvtomobilov/Models/MyWebScraper.cs
@@ -3,7 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -44,10 +47,128 @@ namespace primerjanjeAvtomobilov.Models
                     htmlOglas.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("ResultsAdPrice")).FirstOrDefault().InnerText
                 , @"(\d+.\d+)|^[\d+,\d+]").ToString();
 
-                Console.WriteLine("DEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEELA"+cena);
+                prenesiSliko(imgUrl);
+                //Console.WriteLine(naslov);
+                Console.WriteLine("img url: " + imgUrl);
+                //Console.WriteLine(letnik);
+                //Console.WriteLine(prevozeni);
+                //Console.WriteLine(motor);
+                //Console.WriteLine(menjalnik);
+                //Console.WriteLine("Cena: " + cena);
+            }
+        }
+
+        private static async void GetAvto(string url)
+        {
+            string imgUrl = "", naslov = "", letnik = "", prevozeni = "", motor = "", menjalnik = "", cena;
+            List<HtmlNode> imgNodes, carNodes;
+            string OglasDataLeft, OglasDataRight, Base64OglasDataLeft;
+
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            imgNodes = htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("id", "").Equals("lightgallery")).ToList();
+
+            foreach (var imgNode in imgNodes) // for zanka za pozneje
+            {
+                imgUrl = imgNode.Descendants("p").FirstOrDefault().GetAttributeValue("data-src", "");
             }
 
+            carNodes = htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("OglasData")).ToList();
 
+            cena = Regex.Match(
+                htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("OglasPrice")).First().InnerText
+                , @"(\d+.\d+)|^[\d+,\d+]").ToString();
+
+            foreach (var carNode in carNodes)
+            {
+                OglasDataLeft = carNode.Descendants("div").FirstOrDefault().InnerText;
+                try
+                {
+                    OglasDataRight = carNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("OglasDataRight")).First().InnerText;
+                    //    
+                }
+                catch
+                {
+                    OglasDataRight = "";
+                }
+
+                //zakodiram v base64 ker so nekateri znaki v unicode
+
+                Base64OglasDataLeft = Base64Encode(OglasDataLeft);
+
+                switch (Base64OglasDataLeft)
+                {
+                    case "TGV0bmlrOg==":
+                        letnik = Regex.Match(
+                            OglasDataRight
+                            , @"(\d+.\d+)|^[\d+,\d+]").ToString();
+                        break;
+                    case "UHJldm/vv71lbmkga206==":
+                        prevozeni = OglasDataRight;
+                        break;
+                    case "TW90b3I6":
+                        motor = OglasDataRight;
+                        break;
+                    case "TWVuamFsbmlrOg==":
+                        menjalnik = OglasDataRight;
+                        break;
+                    default:
+                        break;
+                }
+
+
+
+                //Console.WriteLine(OglasDataLeft); // + " " + Base64OglasDataLeft);
+                //Console.WriteLine(OglasDataRight);
+
+
+            }
+            prenesiSliko(imgUrl);
+            //Console.WriteLine(letnik);
+            //Console.WriteLine(prevozeni);
+            //Console.WriteLine(menjalnik);
+            //Console.WriteLine(motor);
+
+
+
+        }
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        private static void prenesiSliko(string url)
+        {
+            string imgName = ComputeSha256Hash(url);
+            if (url == "../_graphics/0.jpg") url = "https://avto.net/_graphics/0.jpg";
+
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(new Uri(url), @"c:\temp\" + imgName + ".png");
+            }
         }
     }
 }
