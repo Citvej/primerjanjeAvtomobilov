26093e502179ee9db416e45e480335158166a4b4
v-cer <v.cernec@gmail.com>
2019-06-05 16:20:52 +0200

    dodani komentarji in manjši popravki pri primerjavi slik

--- primerjanjeAvtomobilov/Models/RazpoznavaAvtomobila.cs
+++ primerjanjeAvtomobilov/Models/RazpoznavaAvtomobila.cs
@@ -17,14 +17,12 @@ namespace primerjanjeAvtomobilov.Models {
             253, 254, 255 };
         #region LBP
 
+        // bool array -> int
         int boolToInt(bool[] arr) {
             double num = 0;
-            for (int i = 0; i < 8; i++) { // on which end is the most significant bit?
+            for (int i = 0; i < 8; i++) { 
                 num += Convert.ToInt32(arr[i]) * Math.Pow(2, 7 - i);
             }
-            /*for (int i = 0; i < 8; i++) { 
-				num += Convert.ToInt32(arr[i]) * Math.Pow(2, i);
-			}*/
             return Convert.ToInt32(num);
         }
 
@@ -35,15 +33,17 @@ namespace primerjanjeAvtomobilov.Models {
             return bin;
         }
 
+        // preveri če je vzorec uniformen
         int isUniform(int pattern) {
             for (int i = 0; i < 58; i++) {
                 if (pattern == uniformPatterns[i]) {
-                    return i;
+                    return i; // vrne index v uniformPatterns (glej zgoraj) na katerem se ta vzorec nahaja
                 }
             }
-            return -1;
+            return -1; // če ni uniformen: -1
         }
 
+        // prejme x in y koordinato središčne točke (sidra). vrne array koordinat točk okolice v pravem vrstnem redu
         int[,] generateCoordinates(int x, int y) {
             int[,] coordinates = new int[,] {
 				/* x--
@@ -91,6 +91,7 @@ namespace primerjanjeAvtomobilov.Models {
         }
 
         List<int> LBP(Image<Gray, byte> img, int cellsize) { // regionSize - npr 16
+            // bin je bool array 
             bool[] bin = new bool[8];
             for (int i = 0; i < 8; i++) {
                 bin[i] = false;
@@ -101,6 +102,24 @@ namespace primerjanjeAvtomobilov.Models {
             int rows = img.Rows;
             int cols = img.Cols;
 
+            // zanka samo do zadnje celice, -1 zaradi tega ker je offset 1 pixel
+            /* brez offseta bi bil segfault
+            
+            brez offseta:
+             0 1 2_______ <-- out of bounds
+             7|x 3
+             6|5 4
+              |
+              |
+
+            offset 1px:
+               _________
+              |0 1 2
+              |7 x 3
+              |6 5 4
+              |
+
+             */
             int lenRows = rows - cellsize - 1;
             int lenCols = cols - cellsize - 1;
 
@@ -116,21 +135,24 @@ namespace primerjanjeAvtomobilov.Models {
                 img.Resize(img.Width, tmpsize, Emgu.CV.CvEnum.Inter.Linear);
             }
 
+            // premik po celicah - za začetek se vzame zgornji levi kot
             for (int i = 1; i < lenRows; i += cellsize) {
                 for (int j = 1; j < lenCols; j += cellsize) {
                     histogram = new List<int>(new int[256]);
+                    
+                    // premik po vsakem pikslu znotraj celice, od začetka ki ga določata zunanji zanki do velikosti celice
                     int innerLenX = i + cellsize;
                     int innerLenY = j + cellsize;
                     for (int k = i; k < innerLenX; k++) { // rows
                         for (int l = j; l < innerLenY; l++) { // cols
                             double centreGray = img[k, l].Intensity;
 
-                            // clockwise
-                            int[,] coordinates = generateCoordinates(k, l);
+                            // okolica piksla, od zgoraj levo v smeri urinega kazalca
+                            int[,] coordinates = generateCoordinates(k, l); 
                             for (int m = 0; m < 8; m++) {
                                 int x = coordinates[m, 0];
                                 int y = coordinates[m, 1];
-                                if (img[x, y].Intensity == centreGray)
+                                if (img[x, y].Intensity <= centreGray) // če je piksel <= tistemu na sredini
                                     bin[m] = true;
                             }
 
@@ -161,8 +183,7 @@ namespace primerjanjeAvtomobilov.Models {
             int lenRows = rows - cellsize - 1;
             int lenCols = cols - cellsize - 1;
 
-            // resize image
-            // Image<Bgr, byte> cpimg = img.Resize(width, height, Emgu.CV.CvEnum.Inter.Linear);
+            // resize 
             int tmpsize;
             if (img.Width - 2 % cellsize != 0) {
                 tmpsize = img.Width + cellsize - (img.Width % cellsize); // +2 or not +2?
@@ -173,6 +194,7 @@ namespace primerjanjeAvtomobilov.Models {
                 img.Resize(img.Width, tmpsize, Emgu.CV.CvEnum.Inter.Linear);
             }
 
+            // zanke iste kot prej
             for (int i = 1; i < lenRows; i += cellsize) {
                 for (int j = 1; j < lenCols; j += cellsize) {
                     histogram = new List<int>(new int[59]);
@@ -182,19 +204,18 @@ namespace primerjanjeAvtomobilov.Models {
                         for (int l = j; l < innerLenY; l++) { // cols
                             double centreGray = img[k, l].Intensity;
 
-                            // clockwise
                             int[,] coordinates = generateCoordinates(k, l);
                             for (int m = 0; m < 8; m++) {
                                 int x = coordinates[m, 0];
                                 int y = coordinates[m, 1];
-                                if (img[x, y].Intensity == centreGray)
+                                if (img[x, y].Intensity <= centreGray)
                                     bin[m] = true;
                             }
 
                             // bin to int
                             int num = boolToInt(bin);
-                            if (isUniform(num) != -1) {
-                                histogram[isUniform(num)]++;
+                            if (isUniform(num) != -1) { // je številka ki pride ven uniformna?
+                                histogram[isUniform(num)]++; // indeks v histogramu enak tistemu na katerem se nahaja uniformna številka
                             }
 
                             // reset bin
@@ -220,8 +241,7 @@ namespace primerjanjeAvtomobilov.Models {
             int lenRows = rows - cellsize - 1;
             int lenCols = cols - cellsize - 1;
 
-            // resize image
-            // Image<Bgr, byte> cpimg = img.Resize(width, height, Emgu.CV.CvEnum.Inter.Linear);
+            // resize 
             int tmpsize;
             if (img.Width - 2 % cellsize != 0) {
                 tmpsize = img.Width + cellsize - (img.Width % cellsize);
@@ -240,7 +260,7 @@ namespace primerjanjeAvtomobilov.Models {
                     for (int k = i; k < innerLenX; k++) { // rows
                         for (int l = j; l < innerLenY; l++) { // cols
 
-                            // clockwise
+                            // primerja s tistim ki je oddaljen za d
                             int[,] coordinates = generateCoordinates(k, l);
                             int next = 0;
                             for(int m = 0; m < 8; m++) {
@@ -249,7 +269,7 @@ namespace primerjanjeAvtomobilov.Models {
                                 int y = coordinates[m, 1];
                                 int xNext = coordinates[next, 0];
                                 int yNext = coordinates[next, 1];
-                                if (img[x, y].Intensity == img[xNext, yNext].Intensity)
+                                if (img[x, y].Intensity <= img[xNext, yNext].Intensity)
                                     bin[m] = true;
                             }
 
@@ -269,25 +289,32 @@ namespace primerjanjeAvtomobilov.Models {
 
 
         #region HOG
-        List<double> gradientVector(double[,] power, double[,] orientation, int startX, int startY, int blockSize) {
-            IDictionary<int, double> gradient = new Dictionary<int, double>() {
-                { 0, 0 }, { 20, 0 }, { 40, 0 }, { 60, 0 }, { 80, 0 }, { 100, 0 },
-                { 120, 0 }, { 140, 0 }, { 160, 0 }, { 180, 0 }
-            };
-            double gP;
-            double gO;
 
+        // notranji nivo: ena celica
+        // parametri: arraya za moč & orientacijo, x in y zgornjega levega piksla celice, velikost celice
+        List<double> gradientVector(double[,] power, double[,] orientation, int startX, int startY, int blockSize) {
+            // dictionary ker je lažji za uporabo kot list v tem primeru - ker so indexi lahko 0, 20...180
+            IDictionary<int, double> gradient = new Dictionary<int, double>() {
+                { 0, 0 }, { 20, 0 }, { 40, 0 }, { 60, 0 }, { 80, 0 },
+                { 100, 0 }, { 120, 0 }, { 140, 0 }, { 160, 0 }, { 180, 0 }
+            };
+            double gP; // moč
+            double gO; // orientacija
+
+            // konec zanke, začetek + velikost celice
             int lenX = startX + blockSize;
             int lenY = startY + blockSize;
+
             for (int i = startX; i < lenX; i++) {
                 for (int j = startY; j < lenY; j++) {
                     gP = power[i, j];
                     gO = orientation[i, j];
 
-                    for (int k = 0; k < 180; k += 20) {
+                    // zanka skozi bin array
+                    for (int k = 0; k < 180; k += 20) { // je tu mogoče boljše <= 180?
                         int nextbin = k + 20;
                         // gO equals bin
-                        if (gO == k) { // what to do in case of 0? fix this
+                        if (gO == k) { 
                             gradient[k] += gP;
                             break;
                         }
@@ -300,7 +327,7 @@ namespace primerjanjeAvtomobilov.Models {
                     }
                 }
             }
-            // convert dict to list?
+            // pretvorba dictionary > list 
             List<double> values = new List<double>();
             foreach (KeyValuePair<int, double> bin in gradient) {
                 values.Add(bin.Value);
@@ -309,15 +336,20 @@ namespace primerjanjeAvtomobilov.Models {
             return values;
         }
 
-        // for a region
+        // srednji nivo: ena regija 
+        // parametri: arraya za moč & orientacijo, x in y zgornjega levega piksla regije, velikost celice (npr. 10x10), velikost regije (npr. 2x2)
         List<double> region(double[,] power, double[,] orientation, int startX, int startY, int blockSize, int regionSize) {
             List<double> bins = new List<double>();
             List<double> tmp;
+
             int len = blockSize * regionSize;
+            // premikanje po regiji za 1 celico
             for (int i = startX; i < len; i += blockSize) {
                 for (int j = startY; j < len; j += blockSize) {
+                    // histogram celice
                     tmp = gradientVector(power, orientation, i, j, blockSize);
-                    bins = bins.Concat(tmp).ToList();
+                    // prilepimo rezultat na konec trenutnega seznama
+                    bins = bins.Concat(tmp).ToList(); 
                 }
             }
 
@@ -347,8 +379,7 @@ namespace primerjanjeAvtomobilov.Models {
             // bins 9 > [0|20|40|60|80|100|120|140|160]
             Image<Gray, byte> grayImg = img.Convert<Gray, byte>();
 
-            // resize image
-            // Image<Bgr, byte> cpimg = img.Resize(width, height, Emgu.CV.CvEnum.Inter.Linear);
+            // resize 
             int tmpsize;
             if (grayImg.Width % cellsize != 0) {
                 tmpsize = grayImg.Width + cellsize - (grayImg.Width % cellsize);
@@ -359,7 +390,7 @@ namespace primerjanjeAvtomobilov.Models {
                 grayImg.Resize(grayImg.Width, tmpsize, Emgu.CV.CvEnum.Inter.Linear);
             }
 
-            // preprocessing
+            // predprocesiranje
             // sobel
             Image<Gray, float> sobelX = grayImg.Sobel(1, 0, 1);
             Image<Gray, float> sobelY = grayImg.Sobel(0, 1, 1);
@@ -370,16 +401,16 @@ namespace primerjanjeAvtomobilov.Models {
             double[,] power = new double[rows, columns];
             double[,] orientation = new double[rows, columns];
 
-            // power/orientation vectors
+            // moč in orientacija
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < columns; j++) {
                     double x = sobelX[i, j].Intensity;
                     double y = sobelY[i, j].Intensity;
 
-                    // power vector
+                    // moč: sqrt(Gx^2 + Gy^2)
                     power[i, j] = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)); // values range up to 1000 - why?
 
-                    // orientation vector 
+                    // orientacija: absolutna vrednost atan(Gy/Gx) v stopinjah
                     orientation[i, j] = Math.Abs(Math.Atan2(y, x) * (180 / Math.PI));
                 }
             }
@@ -388,13 +419,15 @@ namespace primerjanjeAvtomobilov.Models {
             //debugBox1.Text = printMatrix(power);
             //debugBox2.Text = printMatrix(orientation);
 
-            // loop through regions
+            // premikanje okvirja skozi regije
             List<double> histogram = new List<double>();
             List<double> tmp = new List<double>();
             int step = cellsize * regions / 2;
             for (int i = 0; i < grayImg.Width; i += step) {
-                for (int j = 0; j < grayImg.Height; j += step) { // would it go out of bounds?
+                for (int j = 0; j < grayImg.Height; j += step) { 
+                    // histogram regije
                     tmp = region(power, orientation, i, j, cellsize, regions);
+                    // prilepimo rezultat na konec trenutnega seznama
                     histogram = histogram.Concat(tmp).ToList();
                 }
             }
